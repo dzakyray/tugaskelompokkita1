a88b483c6a82fed39bc59550f6dc4e5c9189da13
l200220117_DZAKY RAYSSA buntoro <115058694+dzakyray@users.noreply.github.com>
2024-10-14 21:53:46 +0700

    pakai requerment lebih simple
    pakai requerment

---
+++ blog/models.py
@@ -0,0 +1,21 @@
+
+from django.db import models
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
